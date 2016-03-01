2990a346e2f58e97af4387208642e2c9c5774ade
ngnedovsky <ngnedovskiy@help.rustest.ru>
2016-03-01 17:54:43 +0300

    9

--- algs/sedgewick/sedgewick/sorting.cpp
+++ algs/sedgewick/sedgewick/sorting.cpp
@@ -364,10 +364,143 @@ void heapsort( T& a)
     }
 }
 
+template<typename Key, typename Value>
+class binarySearchSt{
+    std::vector<Key> keys;
+    std::vector<Value> vals;
+    int n;
+public:
+    binarySearchSt(int size){
+        keys.reserve(size);
+        vals.reserve(size);
+    }
+    int size(){ 
+        return n; 
+    }
+    Value get(Key key){
+        if(n == 0) return NULL;
+        int i = rank2(key);
+        if(i < n && keys[i] == key)
+            return vals[i];
+        else
+            return NULL;
+    }
+    int rank2(Key key, int low, int high){
+        if(high < low)
+            return low;
+        int mid = low + (high - low) / 2;
+        int cmp = 0;
+        if(key < keys[mid]) cmp = -1;
+        else if(key > keys[mid]) cmp = 1;
+        else cmp = 0;
+
+        if(cmp < 0){
+            return rank2(key, low, mid - 1);
+        }else if(cmp > 0){
+            return rank2(key, mid + 1, high);
+        }else{
+            return mid;
+        }
+    }
+    int rank2(Key key){
+        int low = 0;
+        int high = n - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            
+            int cmp = 0;
+            if(key == keys[mid]) cmp = 0;
+            else if(key < keys[mid]) cmp = -1;
+            else cmp = 1;
+
+            if(cmp < 0) high = mid - 1;
+            else if(cmp > 0) low = mid + 1;
+            else return mid;
+        }
+        return low;
+    }
+    void put(Key key, Value val){
+        int i = rank2(key);
+        if(i < n && keys[i] == key){
+            vals[i] = val;
+            return;
+        }
+        for(int j = n; j > i; j--){
+            keys[j] = keys[j - 1];
+            vals[j] = vals[j - 1];
+        }
+        keys[i] = key;
+        vals[i] = val;
+        n++;
+    }
+};
+
+//363
+template <typename Key, typename Value>
+class BST{
+    class node3{
+        Key key;
+        Value val;
+        node3& left;
+        node3& right;
+        int n;
+        node3(Key key, Value val, int n){
+            this->key = key;
+            this->val = val;
+            this->n = n;
+        }
+    };
+    node3 root;
+    int size(node3* x){
+        if(x == nullptr) return 0;
+        else return x->n;
+    }
+    int size(){
+        return size(root);
+    }
+    //-----------------
+    Value get(node3* x, Key key){
+        if(x == nullptr) return nullptr;
+        int cmp = 0;
+        
+        if(key < x->key) cmp = -1;
+        else if(key > x->key) cmp = 1;
+        else cmp = 0;
+
+        if(cmp < 0) return get(x->left, key);
+        else if(cmp > 0) return get(x->right, key);
+        else return x->val;
+
+    }
+    Value get(Key key){
+        return get(root, key);
+    }
+    //---------------
+    void put(Key key, Value val){
+        root = put(root, key, val);
+    }
+    node3* put(node3* x, Key key, Value val){
+        if(x == nullptr) return new node3(key, val, 1);
+        int cmp = 0;
+        
+        if(key < x->key) cmp = -1;
+        else if(key > x->key) cmp = 1;
+        else cmp = 0;
+
+        if(cmp < 0) x->left = put(x->left, key, val);
+        else if(cmp > 0) x->right = put(x->right, key, val);
+        else x->val = val;
+
+        x->n = size(x->left) + size(x->right) + 1;
+        return x;
+    }
+};
+
 void list999(){
     std::vector<int> vints;
     std::string file_path("..\\..\\..\\files\\100-bytes.txt");
     read_file(vints, file_path);
     //MaxPQ pq;
     //pq.insert()
+
 }
